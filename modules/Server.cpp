373355289f7cd3beffca5263d9237f90407b314a
nakato156 <christianvelasces@gmail.com>
2022-06-13 22:14:06 -0500

    create files

---
+++ modules/Server.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <winsock2.h>
+
+using namespace std;
+
+class Server {
+    public:
+    WSADATA WSAData;
+    SOCKET server, client;
+    SOCKADDR_IN serverAddr, clientAddr;
+    char buffer[1024];
+    Server() {
+        WSAStartup(MAKEWORD(2,0), &WSAData);
+        server = socket(AF_INET, SOCK_STREAM, 0);
+
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(5555);
+
+        bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+        listen(server, 0);
+
+        cout << "Esperando al otro jugador..." << endl;
+        int clientAddrSize = sizeof(clientAddr);
+        if((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
+        {
+            cout << "Jugador Conectado!" << endl;
+        }
+    }
+    
+    string Rec() {
+      recv(client, buffer, sizeof(buffer), 0);
+      cout << "El cliente dice: " << buffer << endl;
+      memset(buffer, 0, sizeof(buffer));
+    }
+
+    void Send() {
+        cout<<"Escribe el mensaje a enviar: ";
+        cin>>this->buffer;
+        send(client, buffer, sizeof(buffer), 0);
+        memset(buffer, 0, sizeof(buffer));
+        cout << "Mensaje enviado!" << endl;
+    }
+
+    void Close() {
+        closesocket(client);
+        cout << "Socket cerrado, cliente desconectado." << endl;
+    }
+};
+
+void StartServer() {
+    Server *Servidor = new Server();
+    while(true)
+    {
+        Servidor->Rec();
+        Servidor->Send();
+    }
+}
